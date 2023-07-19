5f0418a8d59da2929d26264db13b5b7c3d92391e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-19 15:49:12 -0600

    create merchants index view

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -7,11 +7,18 @@ RSpec.describe '/merchants', type: :feature do
 
   describe 'Merchants Index' do
     it 'lists names of merchants' do 
-      # expect(page).to have_content()
+      expect(page).to have_content('Merchants')
+      within(".merchants") do
+        expect(page).to have_content("Schroeder-Jerde")
+        expect(page).to have_content("Klein, Rempel and Jones")
+        expect(page).to have_content("Willms and Sons")
+      end
     end
-
+    
     it 'names of merchants link to merchants' do 
-      
+      expect(page).to have_link("Schroeder-Jerde", href: '/merchants/1')
+      expect(page).to have_link("Klein, Rempel and Jones", href: '/merchants/2')
+      expect(page).to have_link("Willms and Sons", href: '/merchants/3')
     end
   end
 end
