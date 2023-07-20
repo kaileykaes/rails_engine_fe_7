b47eccf064523e9df184509c343d58eeed4eac84
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-19 21:06:34 -0600

    create merchant show page

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -11,9 +11,30 @@ RSpec.describe 'Merchant Show page', type: :feature do
     end
 
     it 'has name of merchant' do 
-      visit "merchants/1"
+      visit "/merchants/1"
 
       expect(page).to have_content("Schroeder-Jerde")
+    end
+
+    it 'has descriptions for attributes' do 
+      visit "/merchants/1"
+
+      within(".items") do 
+        expect(page).to have_content("Name:")
+        expect(page).to have_content("Description:")
+        expect(page).to have_content("Unit Price:")
+      end
+    end
+
+    it 'has items of merchant' do 
+      visit "/merchants/1" do 
+
+        within(".items") do 
+          expect(page).to have_content("Item Nemo Facere")
+          expect(page).to have_content("Numquam officiis reprehenderit eum ratione neque tenetur.")
+          expect(page).to have_content("Item Ea Voluptatum")
+        end
+      end
     end
   end
 end
